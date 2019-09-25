a24ca0ef0a82376c7d58bddf023c7249ef108a7b
chris2in <38121755+chris2in@users.noreply.github.com>
2019-09-24 22:16:07 -0500

    Update lab1a.m

--- lab1/lab1a.m
+++ lab1/lab1a.m
@@ -1,13 +1,13 @@
-function [d,t] = lab1a(np,nd)
-if(nargin<1), np = 1e7; nd =10; end 
+function [C,t] = lab1a(np,nd)
+if(nargin<1), np = 1e7; nd =2; end 
 
 A = rand(np,nd); B = randn(np,nd);
-d = zeros(np,1);
+C = zeros(np,1);
 tic;
 for i = 1:np
     for j = 1:nd
-        d(i) = d(i) + (B(i,j)-A(i,j)).^2;
+        C(i) = C(i) + (B(i,j)-A(i,j)).^2;
     end 
-    d(i) = sqrt(d(i));
+    C(i) = sqrt(C(i));
 end
 t= toc;
