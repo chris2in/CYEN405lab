4215c7a5f012278dc39a9f7eff72b714a4cdc417
chris2in <38121755+chris2in@users.noreply.github.com>
2019-11-01 00:41:27 -0500

    Create lab4_2d.py

---
+++ lab4/lab4_2d.py
@@ -0,0 +1,13 @@
+"""
+Lab4 Q2D : download files from an amazon s3 bucket
+
+"""
+import boto3
+import botocore
+            
+s3= boto3.resource('s3')
+filename = 'myREADME.md'
+
+bucket_name = 'yli027lab42bucket'
+key = 'README.md'
+s3.Bucket(bucket_name).download_file(key,filename)
