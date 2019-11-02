6988756738c207bdc266498710ebefb6943d91f7
chris2in <38121755+chris2in@users.noreply.github.com>
2019-11-01 22:37:30 -0500

    Create lab4_3a.py

---
+++ lab4/lab4_3a.py
@@ -0,0 +1,38 @@
+"""
+Lab4 Q2f : retrieve a bucket policy
+"""
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2', endpoint_url="http://localhost:8000")
+
+
+table = dynamodb.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+print("Table status:", table.table_status)
